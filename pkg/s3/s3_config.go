075613ee7d42816c37fc0119c7b7cdf08f141509
maciejas22 <motorosas2012@gmail.com>
2024-10-03 22:20:53 +0200

    upkeep: remove dead code

--- pkg/s3/s3_config.go
+++ pkg/s3/s3_config.go
@@ -2,16 +2,13 @@ package s3
 
 import (
 	"context"
-	"log"
 	"log/slog"
-	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	s3Config "github.com/aws/aws-sdk-go-v2/config"
 )
 
 func NewS3Config(ctx context.Context, logger *slog.Logger) (aws.Config, error) {
-	log.Println("AWS_REGION", os.Getenv("AWS_REGION"))
 	cfg, err := s3Config.LoadDefaultConfig(ctx)
 	if err != nil {
 		logger.Error("failed to load S3 config", "error", err)
